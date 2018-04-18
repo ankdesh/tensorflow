a383de9886df628963bd4e8718d2e8db40b45775
ankdesh <a.s.deshwal@gmail.com>
2018-04-18 17:28:41 +0000

    Added Simple Sin Command

---
+++ tensorflow/contrib/lite/kernels/sin.cc
@@ -0,0 +1,100 @@
+
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include "tensorflow/contrib/lite/builtin_op_data.h"
+#include "tensorflow/contrib/lite/context.h"
+#include "tensorflow/contrib/lite/kernels/internal/optimized/optimized_ops.h"
+#include "tensorflow/contrib/lite/kernels/internal/quantization_util.h"
+#include "tensorflow/contrib/lite/kernels/internal/reference/reference_ops.h"
+#include "tensorflow/contrib/lite/kernels/internal/tensor.h"
+#include "tensorflow/contrib/lite/kernels/kernel_util.h"
+#include "tensorflow/contrib/lite/kernels/op_macros.h"
+
+namespace tflite {
+namespace ops {
+namespace builtin {
+namespace sin {
+
+// This file has three implementation of Add.
+enum KernelType {
+  kReference,
+  kGenericOptimized,  // Neon-free
+  kNeonOptimized,
+};
+
+constexpr int kInputTensor1 = 0;
+constexpr int kInputTensor2 = 1;
+constexpr int kOutputTensor = 0;
+
+struct OpData {
+  int my_data[10];
+};
+
+void* Init(TfLiteContext* context, const char* buffer, size_t length) {
+  auto* data = new OpData;
+  for (int i = 0; i < 10; i ++){
+    data->my_data[i] = i;
+  }
+  return data;
+}
+
+void Free(TfLiteContext* context, void* buffer) {
+  delete reinterpret_cast<OpData*>(buffer);
+}
+
+TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
+using namespace tflite;
+  TF_LITE_ENSURE_EQ(context, NumInputs(node), 1);
+  TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
+
+  TfLiteTensor* input = GetInput(context, node, 0);
+  TfLiteTensor* output = GetOutput(context, node, 0);
+
+  int num_dims = NumDimensions(input);
+
+  TfLiteIntArray* output_size = TfLiteIntArrayCreate(num_dims);
+  for (int i=0; i<num_dims; ++i) {
+    output_size->data[i] = input->dims->data[i];
+  }
+
+  return context->ResizeTensor(context, output, output_size);
+}
+
+TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
+using namespace tflite;
+  TfLiteTensor* input = GetInput(context, node,0);
+  TfLiteTensor* output = GetOutput(context, node,0);
+
+  float* input_data = input->data.f;
+  float* output_data = output->data.f;
+
+  size_t count = 1;
+  int num_dims = NumDimensions(input);
+  for (int i = 0; i < num_dims; ++i) {
+    count *= input->dims->data[i];
+  }
+
+  for (size_t i=0; i<count; ++i) {
+    output_data[i] = sin(input_data[i]);
+  }
+  return kTfLiteOk;
+}
+
+}  // namespace add
+
+
+}  // namespace builtin
+}  // namespace ops
+}  // namespace tflite
